173e0478e0ce161cb3f575719d2b03caef5413c5
Joshua Morton <joshua.morton13@gmail.com>
2024-04-05 20:38:25 -0700

    Add first argument to the arg parser for the cli.
    This doesn't do much yet, but it does check for the existence of a
    config file!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use rusqlite::{Connection, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+#[command(about="Lets you tag and query music stored in spotify.")]
+struct Cli {
+    #[arg(short, long, default_value = "stag.toml")]
+    config_file: String,
+}
+
 
 fn main() {
+    let cli = Cli::parse();
     println!("Hello, world!");
 }
