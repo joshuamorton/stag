bf5de230fb31d89885b66245923f4db404440042
Joshua Morton <joshua.morton13@gmail.com>
2024-04-05 21:51:19 -0700

    Add actual data to the config file.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use clap::Parser;
 
+mod settings;
+
 #[derive(Parser)]
 #[command(version, about, long_about=None)]
-#[command(about="Lets you tag and query music stored in spotify.")]
+#[command(about = "Lets you tag and query music stored in spotify.")]
 struct Cli {
     #[arg(short, long, default_value = "stag.toml")]
     config_file: String,
 }
 
-
 fn main() {
     let cli = Cli::parse();
-    println!("Hello, world!");
+    let cfg = settings::StagConfig::from_file(&cli.config_file);
+    match cfg {
+        Err(e) => println!("{}", e),
+        Ok(x) => println!("{:?}", x),
+    }
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,16 +1,17 @@
-use config::{Config, File, ConfigError};
+use config::{Config, ConfigError, File};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct StagConfig {
-
+    pub client_id: String,
+    pub client_secret: String,
 }
 
 impl StagConfig {
     pub fn from_file(path: &str) -> Result<Self, ConfigError> {
-        return Config::builder().add_source(File::with_name(path)).build()?.try_deserialize()
+        return Config::builder()
+            .add_source(File::with_name(path))
+            .build()?
+            .try_deserialize();
     }
-
 }
-
-
